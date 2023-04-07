c446c01bc225713df8af9c64c24b7e15e083fea9
isobekdev <inavruzov777@gmail.com>
2023-04-07 19:29:52 +0500

    test

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -7,12 +7,10 @@ use Illuminate\Http\Request;
 class HomeController extends Controller
 {
     public function show() {
-        $skills = ['html', 'css', 'php', 'bootstrap', 'java'];
-        $name = 'Isobek';
-        $res = [
-            'name' => $name,
-            'skills' => $skills,
+        $user = [
+            1,2,3,4
         ];
-        return view('hello', $res);
+
+        return count($user);
     }
 }
